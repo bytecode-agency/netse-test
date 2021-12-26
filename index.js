7b6faa4c20a27447b4f545ccceb3593d7e14e7aa
Ankit Tyagi <ankittyaagi@gmail.com>
2021-12-26 16:41:24 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+//import node framework
+var express = require('express');
+
+var app = express();
+
+//Respond with "hello world" for request that hit our root "/"
+app.get('/', function (req, res) {
+    res.send('hello world');
+});
+
+//listen to port 3000 by default
+app.listen(process.env.PORT || 3000);
+
+module.exports = app;
